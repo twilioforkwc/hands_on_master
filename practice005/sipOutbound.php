a096beb395d73df6feafdbb326d000b3442442e0
haruka.endo@kddi-web.com <ae104r8buc@ae104.secure.ne.jp>
2017-01-20 17:46:34 +0900

    master

---
+++ practice005/sipOutbound.php
@@ -0,0 +1,28 @@
+#!/usr/local/bin/php-cgi-7.0
+<?php
+//必要なライブラリを取り込む
+require_once '../vendor/autoload.php'; // Loads the library
+use Twilio\Rest\Client;
+
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+
+// Your Account Sid and Auth Token from twilio.com/user/account
+$accountSid = 'ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+$authToken  = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+$client = new Client($sid, $token);
+
+//発信元はSIP端末のみなので、SIPドメインの指定を行う。
+$fromNumber = "+81 yourTwilionumber";
+//宛先から番号を抽出する。
+$toNumber = $_REQUEST['To'];
+$char = "@";
+$str = substr($toNumber, 0, strcspn($toNumber, $char));
+$cut = 5;
+$number = substr( $str , $cut , strlen($str)-$cut );
+?>
+<Response>
+  <Dial callerId="<?php echo($fromNumber)?>">
+    <Number>+81<?php echo($number)?></Number>
+  </Dial>
+</Response>
